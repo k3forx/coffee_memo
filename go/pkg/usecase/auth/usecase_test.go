11745e728297fdad3a38836cd193689136f98ed1
Kanata Miyahana <afiri.bupa@gmail.com>
2022-04-23 22:45:15 +0900

    add tests

--- go/pkg/usecase/auth/usecase_test.go
+++ go/pkg/usecase/auth/usecase_test.go
@@ -135,3 +135,135 @@ func TestUsecase_SignUp(t *testing.T) {
 		})
 	}
 }
+
+func TestUsecase_LogIn(t *testing.T) {
+	t.Parallel()
+
+	const (
+		email = "test@test.com"
+	)
+
+	returnedUser := model.User{
+		ID:       1,
+		Email:    email,
+		Password: "$2a$10$xrYg1iRdRsWsqRLfLuqFaOV6iI7mFCDbF8pZYWbbUUTaWJqBcQVC2",
+	}
+
+	cases := map[string]struct {
+		setup func(ctrl *gomock.Controller) inject.Injector
+		in    auth.LogInInput
+		out   *auth.LogInOutput
+		res   *result.Result
+	}{
+		"success": {
+			setup: func(ctrl *gomock.Controller) inject.Injector {
+				injector := inject.NewMockInjector(ctrl)
+
+				userReader := injector.Reader.User.(*readerMock.MockUser)
+				userReader.EXPECT().GetByEmail(gomock.Any(), email).Return(returnedUser, nil)
+
+				return injector
+			},
+			in: auth.LogInInput{
+				Email:    email,
+				Password: "testtest",
+			},
+			out: &auth.LogInOutput{
+				User: returnedUser,
+			},
+			res: result.OK(),
+		},
+		"empty_email": {
+			setup: func(ctrl *gomock.Controller) inject.Injector {
+				injector := inject.NewMockInjector(ctrl)
+
+				return injector
+			},
+			in: auth.LogInInput{
+				Password: "testtest",
+			},
+			out: nil,
+			res: result.New(result.CodeBadRequest, "Email: cannot be blank."),
+		},
+		"empty_password": {
+			setup: func(ctrl *gomock.Controller) inject.Injector {
+				injector := inject.NewMockInjector(ctrl)
+
+				return injector
+			},
+			in: auth.LogInInput{
+				Email: email,
+			},
+			out: nil,
+			res: result.New(result.CodeBadRequest, "Password: パスワードは必須項目です."),
+		},
+		"error_in_getting_user": {
+			setup: func(ctrl *gomock.Controller) inject.Injector {
+				injector := inject.NewMockInjector(ctrl)
+
+				userReader := injector.Reader.User.(*readerMock.MockUser)
+				userReader.EXPECT().GetByEmail(gomock.Any(), email).Return(model.User{}, errors.New("server error"))
+
+				return injector
+			},
+			in: auth.LogInInput{
+				Email:    email,
+				Password: "testtest",
+			},
+			out: nil,
+			res: result.Error(),
+		},
+		"user_does_not_found": {
+			setup: func(ctrl *gomock.Controller) inject.Injector {
+				injector := inject.NewMockInjector(ctrl)
+
+				userReader := injector.Reader.User.(*readerMock.MockUser)
+				userReader.EXPECT().GetByEmail(gomock.Any(), email).Return(model.User{}, nil)
+
+				return injector
+			},
+			in: auth.LogInInput{
+				Email:    email,
+				Password: "testtest",
+			},
+			out: nil,
+			res: result.New(result.CodeNotFound, result.CodeNotFound.String()),
+		},
+		"wrong_password": {
+
+			setup: func(ctrl *gomock.Controller) inject.Injector {
+				injector := inject.NewMockInjector(ctrl)
+
+				userReader := injector.Reader.User.(*readerMock.MockUser)
+				userReader.EXPECT().GetByEmail(gomock.Any(), email).Return(returnedUser, nil)
+
+				return injector
+			},
+			in: auth.LogInInput{
+				Email:    email,
+				Password: "wrong pass",
+			},
+			out: nil,
+			res: result.New(result.CodeBadRequest, "パスワードが違います。"),
+		},
+	}
+
+	for name, c := range cases {
+		c := c
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			ctrl := gomock.NewController(t)
+			u := auth.NewUsecase(c.setup(ctrl))
+
+			out, res := u.LogIn(context.Background(), c.in)
+
+			if diff := cmp.Diff(c.res, res); diff != "" {
+				t.Errorf("LogIn() result mismatch (-want +got):\n%s", diff)
+			}
+			if diff := cmp.Diff(c.out, out); diff != "" {
+				t.Errorf("LogIn() output mismatch (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
